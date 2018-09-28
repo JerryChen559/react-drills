199ba334fbd1a84978479e4a48c5f1bd406fb751
Jerry <kickassmusic4vr@hotmail.com>
2018-09-28 14:35:59 -0500

    app-2 completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -12,13 +12,14 @@ class App extends Component {
   }
 
   render() {
+    let words = this.state.arr.map(word => <div>{word}</div>);
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <h2>{this.state.arr}</h2>
+        <h2>{words}</h2>
       </div>
     );
   }
