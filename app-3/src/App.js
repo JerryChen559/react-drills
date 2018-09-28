af7fbb27a7993b07af500c0385a151033f16ade3
Jerry <kickassmusic4vr@hotmail.com>
2018-09-28 15:14:32 -0500

    completed 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: "",
+      arrayOfWords: [
+        "deer",
+        "bear",
+        "chair",
+        "care",
+        "mouse",
+        "coca-cola",
+        "test",
+        "test2"
+      ]
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
   render() {
+    let wordsOutput = this.state.arrayOfWords
+      .filter(match => match.includes(this.state.input))
+      .map((word, i) => <div key={i}>{word}</div>);
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" onChange={this.onChange} />
+        {wordsOutput}
       </div>
     );
   }
